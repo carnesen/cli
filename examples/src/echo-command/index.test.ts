2203f8de4c0a3ffb88f2c08599f0830388291dfa
Chris Arnesen <chris.arnesen@gmail.com>
2020-07-26 22:56:00 -0700

    Update echo example command to use console API
    Close https://github.com/carnesen/cli/issues/138

--- examples/src/echo-command/index.test.ts
+++ examples/src/echo-command/index.test.ts
@@ -1,11 +1,25 @@
-import { Cli } from '@carnesen/cli';
+import { Cli, ICliOptions, CliNoConsole } from '@carnesen/cli';
 import { echoCommand } from '.';
 
-const cli = Cli(echoCommand);
-
 describe(echoCommand.name, () => {
-	it('" "-joins and returns the provided positional args', async () => {
+	it('" "-joins and console.log\'s the provided positional args', async () => {
+		const spy = jest.fn();
+		const options: ICliOptions = {
+			console: { ...CliNoConsole(), log: spy },
+		};
+		const cli = Cli(echoCommand, options);
 		const returnValue = await cli.api(['foo', 'bar', 'baz']);
-		expect(returnValue).toBe('foo bar baz');
+		expect(returnValue).toBe(undefined);
+		expect(spy).toHaveBeenCalledWith('foo bar baz');
+	});
+
+	it('" "-joins and console.error\'s the provided positional args if --error', async () => {
+		const spy = jest.fn();
+		const options: ICliOptions = {
+			console: { ...CliNoConsole(), error: spy },
+		};
+		const cli = Cli(echoCommand, options);
+		await cli.api(['foo', 'bar', 'baz', '--error']);
+		expect(spy).toHaveBeenCalledWith('foo bar baz');
 	});
 });
