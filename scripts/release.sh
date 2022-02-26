714fea1ccdfff9651f911f6bf0811aa6abba384c
Chris Arnesen <chris.arnesen@gmail.com>
2022-02-26 00:24:42 -0600

    Fix top-level release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -22,7 +22,7 @@ cd packages
 
 cd cli
 npm ci
-./scripts/release.sh "${SEMVER_BUMP}"
+npm run release -- "${SEMVER_BUMP}"
 cd ..
 
 cd cli-examples
@@ -35,12 +35,12 @@ cd cli-website
 npm ci
 npm install @carnesen/cli@latest
 npm install @carnesen/cli-examples@latest
-npm publish
+npm run release
 cd ..
 
 cd cli-website-server
 npm ci
 npm install @carnesen/cli-docs@latest
 npm install @carnesen/cli-website@latest
-npm publish
+npm run release
 cd ..
